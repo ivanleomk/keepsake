eb865ae25e9415847db8f3b2b3f40ff9b747fce5
Ivan <ivanleomk@gmail.com>
2020-08-17 13:53:41 +0800

    Deployment Build

--- Server/app.js
+++ Server/app.js
@@ -5,11 +5,12 @@ const port = 5000
 var cors = require('cors')
 const config = require('./config')['development']['database']
 const bcrypt = require('bcrypt');
+const { json } = require('body-parser')
+var ikea = require('ikea-name-generator');
+
 
 const Pool = require('pg').Pool
 const saltRounds = 10;
-
-
 
 const pool = new Pool({
     user: config['user'],
@@ -27,17 +28,19 @@ app.use(bodyParser.json())
 //TODO: CREATE CARD,SAVE CARD,SHARECARD
 app.post('/loadCards', (req, res) => {
     const { email } = req.body
-    bcrypt.hash(email, saltRounds, function (err, hash)=> {
-        pool.query('SELECT * FROM cards WHERE email = $1', [hash,])
-            .then(({ rows }) => res.send(rows))
-            .catch(err => res.status(300).send(300))
+    pool.query('SELECT * FROM cards WHERE email = $1', [email,])
+    .then(({ rows }) => res.send(rows))
+    .catch(err => {
+        console.log(err)
+        res.status(300).send(300)
     })
+    
     
 })
 
 app.post('/getCard', (req, res) => {
-    const { cardId } = req.body
-    pool.query('select * FROM cards WHERE id = $1', [cardId,])
+    const { id } = req.body
+    pool.query('select * FROM cards WHERE id = $1', [id,])
         .then(({ rows }) => res.send(rows))
         .catch((err) => res.send(err))
     
@@ -45,17 +48,25 @@ app.post('/getCard', (req, res) => {
 
 app.post('/createCard', (req, res) => {
     const { email } = req.body
-    bcrypt.hash(email, saltRounds, function (err, hash)=> {
-        pool.query('INSERT INTO cards WHERE email = $1', [hash,])
-            .then(({ rows }) => res.send(rows))
-            .catch(err => res.status(300).send(300))
-    })
-   
+    const date = new Date()
+    const today = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    const defaultMetadata = JSON.stringify({ items: [] })
+    var name = ikea.getName(false);
+    pool.query('INSERT INTO cards (email,metadata,lastModified,name) VALUES ($1,$2,$3,$4) RETURNING id', [email, defaultMetadata,today,name])
+        .then((data) => {
+            console.log(data)
+            res.send(data.rows)
+        })
+            .catch(err => {
+                console.log(err)
+                res.status(300).send(300)
+            })
 })
 
 app.post('/saveCard', (req, res) => {
-    const { id , metadata } = req.body
-    pool.query('UPDATE cards SET metadata = $1 WHERE id = $2', [metadata,id])
+    const { id, metadata } = req.body
+    const data = JSON.stringify({items:metadata})
+    pool.query('UPDATE cards SET metadata = $1 WHERE id = $2', [data,id])
             .then(({ rows }) => res.send("Ok"))
             .catch(err => res.status(300).send(err))
     
